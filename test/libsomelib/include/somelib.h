69a49154a68a425ac20b8a8cd4e1cf67b7c35fe8
ylilt <ylilt71@gmail.com>
2016-10-12 10:39:39 +0200

    removed unused tests

--- test/libsomelib/include/somelib.h
+++
@@ -1,6 +0,0 @@
-#ifndef _SOMELIB_H
-#define _SOMELIB_H
-
-double somelib_some_func(double d);
-
-#endif /* _SOMELIB_H */
--- test/libsomelib/src/somelib.c
+++
@@ -1,5 +0,0 @@
-#include "somelib.h"
-
-double somelib_some_func(double d) {
-    return 9.9 * d;
-}
